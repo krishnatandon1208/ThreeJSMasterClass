7e362e14b04cc1757fef55ab9ae5c3b1da7094f9
Krishna Tandon <krishnatandon.1208@gmail.com>
2021-02-07 20:05:32 +0530

    Mesh generated

--- 1/script.js
+++ 1/script.js
@@ -5,3 +5,9 @@
 
 //Creating our first scene using THREE js classes
 const scene = new THREE.Scene();
+
+//Creating a red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
