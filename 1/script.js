38cf3e0a0905b77db2aa9846227c0f1e352876e6
Krishna Tandon <krishnatandon.1208@gmail.com>
2021-02-07 20:35:59 +0530

    camera added

--- 1/script.js
+++ 1/script.js
@@ -11,3 +11,16 @@ const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
+
+//Field of view (fov) is the first parameter of the camera and it has
+//static value as of now.
+//Sizes (Aspect Ratio)
+const sizes = {
+	width: 800,
+	height: 600
+};
+
+//Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+//Adding camera to the scene
+scene.add(camera);
