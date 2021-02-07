dd2217e9bfd2b34643160080524aa06fa63b3e74
Krishna Tandon <krishnatandon.1208@gmail.com>
2021-02-07 19:23:47 +0530

    creating a scene

--- 1/script.js
+++ 1/script.js
@@ -1,1 +1,7 @@
-console.log('Hello Three.js');
+//console.log('Hello Three.js');
+//Testing ThreeJS
+
+//console.log(THREE);
+
+//Creating our first scene using THREE js classes
+const scene = new THREE.Scene();
